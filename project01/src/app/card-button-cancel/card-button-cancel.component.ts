b87da98eba86443e18b484c77019400749231993
Vinicius Fillos <vinifillos@gmail.com>
2024-08-26 23:37:05 -0300

    referenciando arquivos de estilos

--- project01/src/app/card-button-cancel/card-button-cancel.component.ts
+++ project01/src/app/card-button-cancel/card-button-cancel.component.ts
@@ -3,21 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-button-cancel',
   templateUrl: './card-button-cancel.component.html',
-  //styleUrl: './card-button-cancel.component.scss'
-  styles: [
-    `
-      .card-button-cancel {
-        padding: 10px;
-        font-size: 16px;
-        text-align: center;
-        margin-top: 10px;
-        border-radius: 25px;
-        background-color: #ff0000;
-        cursor: pointer;
-        font-weight: bold;
-      }
-    `
-  ],
+  styleUrls: ['./card-button-cancel.component.scss', './card-button-cancel2.component.scss'],
 })
 export class CardButtonCancelComponent {
 

02d997538b66441d166b589ff016a0770a821c32
vinfillos <vinifillos@gmail.com>
2025-11-04 21:57:18 -0300

    CSS Style Binding

--- project02/src/app/app.component.ts
+++ project02/src/app/app.component.ts
@@ -6,11 +6,31 @@ import { Component } from '@angular/core'
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  buttonTitle: string = "Título do botão";
-  buttonDisabled: boolean = false;
+  widthButtton1 = '120px';
+  widthButtton2 = '140';
 
-  onButtonClick() {
-    this.buttonTitle = 'New TITLE';
-    this.buttonDisabled = !this.buttonDisabled;
+  stylesObject = {
+    width:'200px',
+    backgroundColor: 'green',
+    color: 'red'
+  };
+
+  updateStyleObjeto() {
+    this.stylesObject.width = '170px';
+    this.stylesObject.backgroundColor = 'lightgreen';
+    console.log('O método não fuciona corretamente xD')
+  }
+
+  updateStyleObjetoCorreto() {
+    this.stylesObject = {
+      width: '170px',
+      backgroundColor: 'lightgreen',
+      color: 'red',
+    }
+    console.log('Esse método sim fuciona corretamente!')
+  }
+
+  protected updateWidth() {
+    this.widthButtton2 = '300';
   }
 }
